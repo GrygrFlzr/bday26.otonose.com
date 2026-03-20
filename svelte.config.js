7047ad431ce862f8ebef431ecccf6366e88e6a72
GrygrFlzr <git@cybeast.dev>
2026-03-20 21:23:14 +0700

    chore: build config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,15 @@
+import { execSync } from 'node:child_process';
 import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		version: {
+			name: execSync('git rev-parse HEAD').toString().trim(),
+			pollInterval: 0
+		}
+	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
 			filename.includes('node_modules') ? undefined : { runes: true }
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,27 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		target: [
+			// firefox140 is currently supported oldest Firefox ESR
+			// so we use Baseline 2024 which specifies versions before it
+			// as Baseline 2025 specifies newer than in-support ESR
+			'chrome130',
+			'edge130',
+			'firefox132',
+			'safari18.2'
+		],
+		reportCompressedSize: false,
+		sourcemap: false,
+		modulePreload: {
+			polyfill: false
+		},
+		rolldownOptions: {
+			output: {
+				comments: false,
+				minify: true
+			}
+		}
+	}
 });
