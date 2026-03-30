92c40effb5e6740b395794aef2e0a16100b147c7
GrygrFlzr <git@cybeast.dev>
2026-03-30 12:36:22 +0700

    feat: canonical url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-	let { children } = $props();
+	import { page } from '$app/state';
+	import type { LayoutProps } from './$types';
+	const { children }: LayoutProps = $props();
+	const canonicalHref = $derived(
+		`https://bday26.otonose.com${page.url.pathname}${page.url.search}`
+	);
 </script>
 
+<svelte:head>
+	<link rel="canonical" href={canonicalHref} />
+</svelte:head>
 {@render children()}
