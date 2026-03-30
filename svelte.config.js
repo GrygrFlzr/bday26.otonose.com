465381a7441bff9a77e44e73fe0c965c2edd997d
GrygrFlzr <git@cybeast.dev>
2026-03-30 12:10:50 +0700

    config: domain and compilation config

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,38 @@
 import { execSync } from 'node:child_process';
+import { relative, sep } from 'node:path';
 import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		prerender: {
+			origin: 'https://bday26.otonose.com'
+		},
 		version: {
 			name: execSync('git rev-parse HEAD').toString().trim(),
 			pollInterval: 0
 		}
 	},
-	vitePlugin: {
-		dynamicCompileOptions: ({ filename }) =>
-			filename.includes('node_modules') ? undefined : { runes: true }
-	}
+	compilerOptions: {
+		modernAst: true,
+		runes: ({ filename }) => {
+			const relativePath = relative(import.meta.dirname, filename);
+			const pathSegments = relativePath.toLowerCase().split(sep);
+			const isExternalLibrary = pathSegments.includes('node_modules');
+			const isKitErrorPagePath = pathSegments
+				.join('/')
+				.endsWith('@sveltejs/kit/src/runtime/components/svelte-5/error.svelte');
+
+			if (isKitErrorPagePath) {
+				return true;
+			} else if (isExternalLibrary) {
+				return undefined;
+			}
+			// user code
+			return true;
+		}
+	},
 };
 
 export default config;
